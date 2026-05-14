25993dbd13d588b40ddc89a2e9083970a62403e4
multicoder599 <newtonmulti@gmail.com>
2026-05-14 18:49:36 +0300

    s

--- models/Product.js
+++ models/Product.js
@@ -2,14 +2,16 @@ const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
+    
+    // 👉 UPDATED: The strict 'enum' list has been completely removed.
+    // Now it will accept any category string you send from the frontend!
     type: { 
         type: String, 
-        required: true,
-        // 👉 UPDATED: Added 'rooms' to support the new booking category
-        enum: ['food', 'drinks', 'wines', 'rooms', 'eatery', 'drink'] 
+        required: true 
     },
+    
     price: { type: Number, required: true },
-    buying_price: { type: Number, default: 0 }, // 👉 ADD THIS LINE
+    buying_price: { type: Number, default: 0 }, 
     stock: { type: Number, default: 0 }
 }, { timestamps: true });
 
